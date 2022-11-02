c62fdfacf4ced0cf23e3e21ed3b9ef223d58ab38
jayakrishnaambavarapu <83640266+jayakrishnaambavarapu@users.noreply.github.com>
2022-11-02 15:14:58 +0530

    Create key-pair.tf

---
+++ key-pair.tf
@@ -0,0 +1,4 @@
+resource "aws_key_pair" "sample-key" {
+  key_name   = "sample-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCuH1hqJC/W5So1s8pfFF+u/vRHmRUj7RvE2EU+VPFo7CHQM7LPh8KwuJ+OYYEseCu7EOxLFu8uhf20poewhSha2PjFFrwbBllwZbUZLG09IiQlpIUUiOBO7n5DG1BTlM1chEjAbGf1yEBr20JZfm2m95XolVNfJ7ruWyAOQzz/ClzOYJhvA0//1RIkJV1CqKpLoXXVFbBNjQh7Z7c32zXojqdODOV8GRD+TYaZybcoaGq0El6ZfPUOdb+6Zl/aQ7EoD97Jt3A6GXfp+Zf1oGGwLRfkHiIxNCJ6o6t9kc59D//jWvOCO07czAr76Gype3apSPQ9gakABq7MUE/qO6zF ubuntu"
+}
